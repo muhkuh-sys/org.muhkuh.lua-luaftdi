e938e4e42420e1009a19a2c274d3a068a730f7e8
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-09-22 17:58:27 +0200

    Create the ".jonchki" folder if it does not exist yet.

--- examples/jonchki/finalizer.lua
+++ examples/jonchki/finalizer.lua
@@ -33,33 +33,39 @@ HOST_DISTRIBUTION_ID=${platform_distribution_id}
 HOST_DISTRIBUTION_VERSION=${platform_distribution_version}
 HOST_CPU_ARCHITECTURE=${platform_cpu_architecture}
 ]])
-local strPackagePath = t:replace_template('${install_base}/.jonchki/package.txt')
-local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
-if tFileError==nil then
-  cLogger:error('Failed to write the package file "%s": %s', strPackagePath, strError)
+local strPackageDir = t:replace_template('${install_base}/.jonchki')
+local tError, strError = pl.dir.makepath(strPackageDir)
+if tError~=true then
+  cLogger:error('Failed to create the folder "%s": %s', strPackageDir, strError)
 else
-  local Archive = archives(cLogger)
-
-  -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
-  local strArchiveExtension
-  local tFormat
-  local atFilter
-  if strDistId=='windows' then
-    strArchiveExtension = 'zip'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
-    atFilter = {}
+  local strPackagePath = pl.path.join(strPackageDir, 'package.txt')
+  local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
+  if tFileError==nil then
+    cLogger:error('Failed to write the package file "%s": %s', strPackagePath, strError)
   else
-    strArchiveExtension = 'tar.gz'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
-    atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
+    local Archive = archives(cLogger)
+
+    -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
+    local strArchiveExtension
+    local tFormat
+    local atFilter
+    if strDistId=='windows' then
+      strArchiveExtension = 'zip'
+      tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
+      atFilter = {}
+    else
+      strArchiveExtension = 'tar.gz'
+      tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
+      atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
+    end
+
+    local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
+    local strArchive = t:replace_template(string.format('${install_base}/../%s-%s%s_${platform_cpu_architecture}.%s', strArtifactVersion, strDistId, strDistVersion, strArchiveExtension))
+    local strDiskPath = t:replace_template('${install_base}')
+    local strArchiveMemberPrefix = strArtifactVersion
+
+    tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
   end
-
-  local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
-  local strArchive = t:replace_template(string.format('${install_base}/../%s-%s%s_${platform_cpu_architecture}.%s', strArtifactVersion, strDistId, strDistVersion, strArchiveExtension))
-  local strDiskPath = t:replace_template('${install_base}')
-  local strArchiveMemberPrefix = strArtifactVersion
-
-  tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
 end
 
 return tResult

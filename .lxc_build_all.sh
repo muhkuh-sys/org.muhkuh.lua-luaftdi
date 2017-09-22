482dd7b8f81b1e1b153e1552d0d21ba808561293
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-09-22 14:47:45 +0200

    Add LXC build scripts.

---
+++ .lxc_build_all.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+. .lxc_build_windows.sh
+. .lxc_build_ubuntu_1404_x64.sh
+. .lxc_build_ubuntu_1404_x86.sh
+. .lxc_build_ubuntu_1604_x64.sh
+. .lxc_build_ubuntu_1604_x86.sh
+. .lxc_build_ubuntu_1704_x64.sh
+. .lxc_build_ubuntu_1704_x86.sh
+. .lxc_build_artefacts.sh

c09dc111876a3add2867cf1d056bc25794fa674e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-07-15 18:09:06 +0200

    Add example to list all devices.

--- examples/jonchki/finalizer.lua
+++ examples/jonchki/finalizer.lua
@@ -6,6 +6,7 @@ t:install{
   ['../eeprom_erase.lua']         = '${install_base}/',
   ['../eeprom_read_bin.lua']      = '${install_base}/',
   ['../eeprom_write_bin.lua']     = '${install_base}/',
+  ['../list_all_devices.lua']     = '${install_base}/',
   ['../test_bitbang_input.lua']   = '${install_base}/',
   ['../test_bitbang_output.lua']  = '${install_base}/'
 }
